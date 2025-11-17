a503f95cc354bed4fa48b6b0316bb40dc7404b7f
ManikMagarSir <np03cy4s250029@heraldcollege.edu.np>
2025-11-17 20:08:51 +0545

    Ultimate changes again

--- script.js
+++ script.js
@@ -14,3 +14,34 @@ setInterval(() => {
 setInterval(() => {
 
 },2000);
+
+let slider = document.getElementById('slider');
+let value = document.getElementById('value');
+let value1 = document.getElementById('value1');
+let slider1 = document.getElementById('slider1');
+
+slider.addEventListener('input', () => {
+  value.textContent = slider.value;
+  if (value.textContent < 30) {
+    value.textContent = "🙄";
+  }
+    else if (value.textContent < 70) {
+    value.textContent = "😃";
+    }
+    else {
+    value.textContent = "🤩";
+    }
+});
+
+slider1.addEventListener('input', () => {
+  value1.textContent = slider1.value;
+    if (value1.textContent < 30) {
+    value1.textContent = "😞";
+    }
+    else if (value1.textContent < 70) {
+    value1.textContent = "😐";
+    }
+    else {
+    value1.textContent = "😁";
+    }
+});
